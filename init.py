6fbbb712deab3c046cea8c8950fff25bd9dea266
Jon Fortescue <jonfortescue@protonmail.com>
2017-06-21 17:34:18 -0700

    Iterative parsing of xml dump in init.py

---
+++ init.py
@@ -0,0 +1,16 @@
+from lxml import etree
+from pymongo import MongoClient
+
+DUMP_FILE_PATH = "dump/enwikivoyage-latest-pages-articles.xml"
+
+print "didn't fuck this up"
+
+client = MongoClient()
+db = client.app
+
+print "made it"
+
+for event, element in etree.iterparse(DUMP_FILE_PATH, tag="{http://www.mediawiki.org/xml/export-0.10/}page"):
+    for child in list(element):
+        print str(child.tag) + ":\t\t" + str(child.text)
+    element.clear()
