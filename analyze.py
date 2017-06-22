7b9a5558f5e1dcde2c832dc9c81971aa69e35af2
Jon Fortescue <jonfortescue@protonmail.com>
2017-06-22 10:06:03 -0700

    Added CSS and fully functional search bar with fast autocomplete.

---
+++ analyze.py
@@ -0,0 +1,11 @@
+import re
+from pymongo import MongoClient
+
+def analyze(page):
+
+
+def analyze_all():
+
+
+if __name__ == '__main__':
+    analyze()
--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import *
-from lxml import etree
 from flask_pymongo import PyMongo
-import pprint
+import sys
+
+# without this, encoding issues cause errors
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
-mongo = PyMongo(app)
+mongo = PyMongo(app) # establish MongoDB connection
+
+def urlizeTitle(title): # switch title to Wikivoyage style title
+    return title.replace(' ','_')
+def titleToUrl(title):
+    return "https://en.wikivoyage.org/wiki/" + urlizeTitle(title)
+def unUrlizeTitle(urlizedTitle):
+    return urlizedTitle.replace('_',' ')
 
 @app.route("/")
 def home():
-    pages = mongo.db.pages.find()
-    return render_template("index.html", title="Wikivoyage Article Status Analyzer", page=pprint.pprint(pages.finde_one()))
+    allPages = mongo.db.pages.find()
+    return render_template("index.html", title="Wikivoyage Article Status Analyzer", pages=allPages)
+
+@app.route("/<pagetitle>")
+def pageDisplay(pagetitle):
+    title = unUrlizeTitle(pagetitle)
+    page = mongo.db.pages.find_one_or_404({"title": title})
+    return render_template("page.html", page=page, url=titleToUrl(title))
