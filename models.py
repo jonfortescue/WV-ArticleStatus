984bfd3b64553afcf8a90b73b61e4679bfc70fff
Jon Fortescue <jonfortescue@protonmail.com>
2018-04-19 10:17:24 -0700

    Added initial class to work on OO stuff

---
+++ models.py
@@ -0,0 +1,6 @@
+class Article:
+    "Articles are basically like special sections"
+    def __init__(self, title, url, text):
+        self.title = title
+        self.url = url
+        self.text = text
