500b7ed166f0c8f033787a1db3ca74fce6bbe072
Jon Fortescue <jonfortescue@protonmail.com>
2018-04-19 00:55:18 -0700

    Add logic to automatically download and unzip the latest dump

--- init.py
+++ init.py
@@ -1,16 +1,27 @@
 # -*- coding: utf-8 -*-
 from lxml import etree
+from pathlib2 import Path
 from pymongo import MongoClient
+import bz2
 import sys
+import urllib
 
 # without this, encoding issues cause errors
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
- # the file path of the semimonthly dump
- # obtained from https://dumps.wikimedia.org/enwikivoyage/latest/enwikivoyage-latest-pages-articles.xml.bz2
- # TODO: automate getting this dump file
+# the file path of the semimonthly dump
+# obtained from https://dumps.wikimedia.org/enwikivoyage/latest/enwikivoyage-latest-pages-articles.xml.bz2
 DUMP_FILE_PATH = "dump/enwikivoyage-latest-pages-articles.xml"
+if not Path(DUMP_FILE_PATH).is_file():
+    print "No dump found. Downloading latest..."
+    compressed_dump_path = "dump/enwikivoyage-latest-pages-articles.xml.bz2"
+    urllib.urlretrieve("https://dumps.wikimedia.org/enwikivoyage/latest/enwikivoyage-latest-pages-articles.xml.bz2", compressed_dump_path)
+    print "Dump downloaded. Extracting..."
+    with open(DUMP_FILE_PATH, 'wb') as dump_file, bz2.BZ2File(compressed_dump_path, 'rb') as dump_bz2:
+        for data in iter(lambda : dump_bz2.read(100 * 1024), b''):
+            dump_file.write(data)
+    print "Dump extracted."
 
 # connect with mongodb before doing anything else
 client = MongoClient()
